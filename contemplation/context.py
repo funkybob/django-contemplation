ff2a326e0cbf36c32b1fb7a67836a673b55b4e98
Curtis Maloney <curtis@tinbrain.net>
2013-09-14 14:36:02 +1000

    Added builtins to Context

--- contemplation/context.py
+++ contemplation/context.py
@@ -4,6 +4,9 @@ try:
 except ImportError: # Py < 3
     from chainmap import ChainMap
 
+BUILTINS = {'True': True, 'False': False, 'None': None}
+
 class Context(ChainMap):
-    pass
+    def __init__(self, default=None):
+        super(Context, self).__init__(dict(BUILTINS), default or {})
 
