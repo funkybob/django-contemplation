9c188f5d6d10fc3c847f1d665857c79515675f0f
Curtis Maloney <curtis@tinbrain.net>
2013-09-14 16:34:19 +1000

    Add support for Context.push as a context manager

--- contemplation/context.py
+++ contemplation/context.py
@@ -6,7 +6,27 @@ except ImportError: # Py < 3
 
 BUILTINS = {'True': True, 'False': False, 'None': None}
 
+class ContextDict(dict):
+    def __init__(self, context, *args, **kwargs):
+        super(ContextDict, self).__init__(*args, **kwargs)
+        context.maps.insert(0, self)
+        self.context = context
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.context.pop()
+
 class Context(ChainMap):
     def __init__(self, default=None):
         super(Context, self).__init__(dict(BUILTINS), default or {})
+
+    def push(self, *args, **kwargs):
+        return ContextDict(self, *args, **kwargs)
+
+    def pop(self):
+        if len(self.maps) < 2:
+            raise IndexError
+        return self.maps.pop(0)
 
