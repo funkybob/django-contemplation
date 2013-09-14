9e1e3e182920710f836067905746fb030d01162b
Curtis <curtis@tinbrain.net>
2013-09-14 20:15:17 +1000

    Add more tests, including "should fail" tests

--- tests/render.py
+++ tests/render.py
@@ -1,11 +1,11 @@
 
 import unittest
 
-from contemplation import Template, Context
+from contemplation import Template, Context, TemplateSyntaxError
 
 class RenderTests(unittest.TestCase):
 
-    TEST_CASES = (
+    GOOD_CASES = (
         # Raw text
         ('Hello!', {}, 'Hello!'),
         # Basic variable
@@ -16,6 +16,7 @@ class RenderTests(unittest.TestCase):
         ('{{ foo.bar }}', {'foo': {'bar': 'baz'}}, 'baz'),
         ('{{ foo.1 }}', {'foo': ['a', 'b']}, 'b'),
         ('{{ foo.title }}', {'foo': 'something'}, 'Something'),
+        ('{{ foo }}', {'foo': lambda : 'bar'}, 'bar'),
         # XXX multi-level lookups
         # Comments
         ('abc{# comment #}def', {}, 'abcdef'),
@@ -31,15 +32,39 @@ class RenderTests(unittest.TestCase):
 
         # Nested tags
         ('{% for x in y %}{% with b=x %}{{ b }}{% endwith %}{% endfor %}', {'y': '123'}, '123'),
+
+        # Literal strings are permitted inside variables
+        (r'{{ "fred" }}', {}, "fred"),
+        #(r'{{ "\"fred\"" }}', {}, "\"fred\""),
+
+        # Something that starts like a number but has an extra lookup works as a lookup.
+        ("{{ 1.2.3 }}", {"1": {"2": {"3": "d"}}}, "d"),
+        ("{{ 1.2.3 }}", {"1": {"2": ("a", "b", "c", "d")}}, "d"),
+        ("{{ 1.2.3 }}", {"1": (("x", "x", "x", "x"), ("y", "y", "y", "y"), ("a", "b", "c", "d"))}, "d"),
+        ("{{ 1.2.3 }}", {"1": ("xxxx", "yyyy", "abcd")}, "d"),
+        ("{{ 1.2.3 }}", {"1": ({"x": "x"}, {"y": "y"}, {"z": "z", "3": "d"})}, "d"),
+
     )
 
     def test_good(self):
-        for tmpl, ctx, output in self.TEST_CASES:
+        for tmpl, ctx, output in self.GOOD_CASES:
             t = Template(tmpl)
             c = Context(ctx)
             o = t.render(c)
             self.assertEqual(o, output)
 
+    BAD_CASES = (
+        ("{{ va>r }}", TemplateSyntaxError),
+        ("{{ (var.r) }}", TemplateSyntaxError),
+        ("{{ sp%am }}", TemplateSyntaxError),
+        ("{{ eggs! }}", TemplateSyntaxError),
+        ("{{ moo? }}", TemplateSyntaxError),
+    )
+
+    def test_bad(self):
+        for tmpl, exc in self.BAD_CASES:
+            with self.assertRaises(exc):
+                t = Template(tmpl)
 
 if __name__ == '__main__':
     unittest.main()
